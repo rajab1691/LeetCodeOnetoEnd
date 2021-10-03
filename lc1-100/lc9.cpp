9a428e5b3be30ee816aad06c2192dd9aace1710f
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-10-04 01:22:20 +0530

    Update lc9.cpp

--- lc1-100/lc9.cpp
+++ lc1-100/lc9.cpp
@@ -1,1 +1,30 @@
+// https://leetcode.com/problems/palindrome-number
 
+/*
+    T.C= O(N)
+    S.C= const
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        
+        int ans=0;
+        int num=x;
+        while(x>0){
+            int rem=x%10;
+            if(INT_MAX/10>=ans){
+                ans=ans*10;
+            }else{
+                return false;
+            }
+            ans+=rem;
+            x/=10;
+        }
+        if(ans==num)return true;
+        return false;
+    }
+};
