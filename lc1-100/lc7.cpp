ce62c619fb9735eeb1582c8c0262fe6c660cbd32
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-10-04 01:20:14 +0530

    Update lc7.cpp

--- lc1-100/lc7.cpp
+++ lc1-100/lc7.cpp
@@ -1,1 +1,29 @@
+// https://leetcode.com/problems/reverse-integer
+/*
+     T.C= O(N)
+     S.C= const
+*/
 
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int num=abs(x);
+        int ans=0;
+        while(num>0){
+            int rem=num%10;
+            if(INT_MAX/10>=ans){
+                ans=ans*10;
+            }else{
+                return 0;
+            }
+            ans=ans+rem;
+            num/=10;
+        }
+        if(x<0){
+            ans=ans*(-1);
+        }
+        if(ans>INT_MAX or ans<INT_MIN)return 0;
+        return ans;
+    }
+};
