678fed523c3e7c027190fca804efb8a7a01afeb5
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-10-03 21:47:10 +0530

    Update lc1.cpp

--- lc1-100/lc1.cpp
+++ lc1-100/lc1.cpp
@@ -1,1 +1,27 @@
+// https://leetcode.com/problems/two-sum/
 
+/*
+    -> If the array is Sorted then we can use two pointer approach
+    -> Optimal approach- Use hashmap
+     T.C and S.C =O(n)
+*/
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>res;
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            int temp=target-nums[i];
+            if(mp.find(temp)!=mp.end()){
+                res.push_back(i);
+                res.push_back(mp[temp]);
+            }else{
+                mp[nums[i]]=i;
+            }
+        }
+        return res;
+    }
+};
